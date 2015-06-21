356c89b6f56825de0217373c0232932470bf94a0
williamg42 <willig2@vt.edu>
2015-06-21 16:50:55 -0400

    Working on code

--- AGV-Code/Remote Operated/Controller-Firmware/Controller-firmware.ino
+++ AGV-Code/Remote Operated/Controller-Firmware/Controller-firmware.ino
@@ -1,22 +1,12 @@
-#include <PS2X_lib.h>  //for v1.6
+#include <PS2X_lib.h>  
+#include "crc.h" 
 
-/******************************************************************
- * set pins connected to PS2 controller:
- *   - 1e column: original 
- *   - 2e colmun: Stef?
- * replace pin numbers by the ones you use
- ******************************************************************/
-#define PS2_DAT        13  //14    
-#define PS2_CMD        11  //15
-#define PS2_SEL        10  //16
-#define PS2_CLK        12  //17
 
-/******************************************************************
- * select modes of PS2 controller:
- *   - pressures = analog reading of push-butttons 
- *   - rumble    = motor rumbling
- * uncomment 1 of the lines for each mode selection
- ******************************************************************/
+#define PS2_DAT        13     
+#define PS2_CMD        11  
+#define PS2_SEL        10  
+#define PS2_CLK        12  
+
 //#define pressures   true
 #define pressures   false
 //#define rumble      true
@@ -31,10 +21,13 @@ PS2X ps2x; // create PS2 Controller Class
 int error = 0;
 byte type = 0;
 byte vibrate = 0;
-
+unsigned short checksum = 0;
+char message[23];
+ 
 void setup(){
  
   Serial.begin(57600);
+  crcInit();
   
   delay(300);  //added delay to give wireless ps2 module some time to startup, before configuring it
    
@@ -85,40 +78,45 @@ void setup(){
 }
 
 void loop() {
-  /* You must Read Gamepad to get new values and set vibration values
-     ps2x.read_gamepad(small motor on/off, larger motor strenght from 0-255)
-     if you don't enable the rumble, use ps2x.read_gamepad(); with no values
-     You should call this at least once a second
-   */  
-  if(error == 1) //skip loop if no controller found
-    return; 
+
+  if(error == 1) 
+   {
+    //Safe mode for RC system, sets speed to 0;
+   }
   
   else { //DualShock Controller
     ps2x.read_gamepad(false, vibrate); //read controller and set large motor to spin at 'vibrate' speed
 
-    ps2x.Button(PSB_START);    
-    ps2x.Button(PSB_SELECT);    
-    ps2x.Button(PSS_LY);    
-    ps2x.Button(PSS_LX);    
-    ps2x.Button(PSS_RY);    
-    ps2x.Button(PSS_RX);    
-    ps2x.Button(PSB_PAD_UP);    
-    ps2x.Button(PSB_PAD_DOWN);    
-    ps2x.Button(PSB_PAD_LEFT);    
-    ps2x.Button(PSB_PAD_RIGHT);    
-    ps2x.Button(PSB_TRIANGLE);
-    ps2x.Button(PSB_CIRCLE);
-    ps2x.Button(PSB_SQUARE);
-    ps2x.Button(PSB_CROSS);
-    ps2x.Button(PSB_L1);
-    ps2x.Button(PSB_L2);
-    ps2x.Button(PSB_L3);
-    ps2x.Button(PSB_R1);
-    ps2x.Button(PSB_R2);
-    ps2x.Button(PSB_R3);
+    message[0] = 0;
+    message[1] = ps2x.Button(PSB_START);    
+    message[2] = ps2x.Button(PSB_SELECT);    
+    message[3] = ps2x.Button(PSS_LY);    
+    message[4] = ps2x.Button(PSS_LX);    
+    message[5] = ps2x.Button(PSS_RY);    
+    message[6] = ps2x.Button(PSS_RX);    
+    message[7] = ps2x.Button(PSB_PAD_UP);    
+    message[8] = ps2x.Button(PSB_PAD_DOWN);    
+    message[9] = ps2x.Button(PSB_PAD_LEFT);    
+    message[10] = ps2x.Button(PSB_PAD_RIGHT);    
+    message[11] = ps2x.Button(PSB_TRIANGLE);
+    message[12] = ps2x.Button(PSB_CIRCLE);
+    message[13] = ps2x.Button(PSB_SQUARE);
+    message[14] = ps2x.Button(PSB_CROSS);
+    message[15] = ps2x.Button(PSB_L1);
+    message[16] = ps2x.Button(PSB_L2);
+    message[17] = ps2x.Button(PSB_L3);
+    message[18] = ps2x.Button(PSB_R1);
+    message[19] = ps2x.Button(PSB_R2);
+    message[20] = ps2x.Button(PSB_R3);
 
-       
 
+
+   checksum = crcFast((unsigned char *)message, 21);
+
+   message[21] =   (checksum >> 8);     
+   message[22] =   (checksum);
+
+   Serial.write(message, 23);
 
   }
   delay(50);  
