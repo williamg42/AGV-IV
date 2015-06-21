c71faee3f989bd6bd8e17a170e4208ea1fd33d16
williamg42 <willig2@vt.edu>
2015-06-21 13:35:09 -0400

    Added CRC checksum to Controller Fireware

--- AGV-Code/Remote Operated/Controller-Firmware/Controller-firmware.ino
+++ AGV-Code/Remote Operated/Controller-Firmware/Controller-firmware.ino
@@ -55,10 +55,7 @@ void setup(){
 	  Serial.println("true)");
 	else
 	  Serial.println("false");
-    Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
-    Serial.println("holding L1 or R1 will print out the analog stick values.");
-    Serial.println("Note: Go to www.billporter.info for updates and to report bugs.");
-  }  
+   }  
   else if(error == 1)
     Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
    
@@ -96,94 +93,33 @@ void loop() {
   if(error == 1) //skip loop if no controller found
     return; 
   
-  if(type == 2){ //Guitar Hero Controller
-    ps2x.read_gamepad();          //read controller 
-   
-    if(ps2x.ButtonPressed(GREEN_FRET))
-      Serial.println("Green Fret Pressed");
-    if(ps2x.ButtonPressed(RED_FRET))
-      Serial.println("Red Fret Pressed");
-    if(ps2x.ButtonPressed(YELLOW_FRET))
-      Serial.println("Yellow Fret Pressed");
-    if(ps2x.ButtonPressed(BLUE_FRET))
-      Serial.println("Blue Fret Pressed");
-    if(ps2x.ButtonPressed(ORANGE_FRET))
-      Serial.println("Orange Fret Pressed"); 
-
-    if(ps2x.ButtonPressed(STAR_POWER))
-      Serial.println("Star Power Command");
-    
-    if(ps2x.Button(UP_STRUM))          //will be TRUE as long as button is pressed
-      Serial.println("Up Strum");
-    if(ps2x.Button(DOWN_STRUM))
-      Serial.println("DOWN Strum");
- 
-    if(ps2x.Button(PSB_START))         //will be TRUE as long as button is pressed
-      Serial.println("Start is being held");
-    if(ps2x.Button(PSB_SELECT))
-      Serial.println("Select is being held");
-    
-    if(ps2x.Button(ORANGE_FRET)) {     // print stick value IF TRUE
-      Serial.print("Wammy Bar Position:");
-      Serial.println(ps2x.Analog(WHAMMY_BAR), DEC); 
-    } 
-  }
   else { //DualShock Controller
     ps2x.read_gamepad(false, vibrate); //read controller and set large motor to spin at 'vibrate' speed
-    
-    if(ps2x.Button(PSB_START))         //will be TRUE as long as button is pressed
-      Serial.println("Start is being held");
-    if(ps2x.Button(PSB_SELECT))
-      Serial.println("Select is being held");      
 
-    if(ps2x.Button(PSB_PAD_UP)) {      //will be TRUE as long as button is pressed
-      Serial.print("Up held this hard: ");
-      Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
-    }
-    if(ps2x.Button(PSB_PAD_RIGHT)){
-      Serial.print("Right held this hard: ");
-      Serial.println(ps2x.Analog(PSAB_PAD_RIGHT), DEC);
-    }
-    if(ps2x.Button(PSB_PAD_LEFT)){
-      Serial.print("LEFT held this hard: ");
-      Serial.println(ps2x.Analog(PSAB_PAD_LEFT), DEC);
-    }
-    if(ps2x.Button(PSB_PAD_DOWN)){
-      Serial.print("DOWN held this hard: ");
-      Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
-    }   
+    ps2x.Button(PSB_START);    
+    ps2x.Button(PSB_SELECT);    
+    ps2x.Button(PSS_LY);    
+    ps2x.Button(PSS_LX);    
+    ps2x.Button(PSS_RY);    
+    ps2x.Button(PSS_RX);    
+    ps2x.Button(PSB_PAD_UP);    
+    ps2x.Button(PSB_PAD_DOWN);    
+    ps2x.Button(PSB_PAD_LEFT);    
+    ps2x.Button(PSB_PAD_RIGHT);    
+    ps2x.Button(PSB_TRIANGLE);
+    ps2x.Button(PSB_CIRCLE);
+    ps2x.Button(PSB_SQUARE);
+    ps2x.Button(PSB_CROSS);
+    ps2x.Button(PSB_L1);
+    ps2x.Button(PSB_L2);
+    ps2x.Button(PSB_L3);
+    ps2x.Button(PSB_R1);
+    ps2x.Button(PSB_R2);
+    ps2x.Button(PSB_R3);
 
-    vibrate = ps2x.Analog(PSAB_CROSS);  //this will set the large motor vibrate speed based on how hard you press the blue (X) button
-    if (ps2x.NewButtonState()) {        //will be TRUE if any button changes state (on to off, or off to on)
-      if(ps2x.Button(PSB_L3))
-        Serial.println("L3 pressed");
-      if(ps2x.Button(PSB_R3))
-        Serial.println("R3 pressed");
-      if(ps2x.Button(PSB_L2))
-        Serial.println("L2 pressed");
-      if(ps2x.Button(PSB_R2))
-        Serial.println("R2 pressed");
-      if(ps2x.Button(PSB_TRIANGLE))
-        Serial.println("Triangle pressed");        
-    }
+       
 
-    if(ps2x.ButtonPressed(PSB_CIRCLE))               //will be TRUE if button was JUST pressed
-      Serial.println("Circle just pressed");
-    if(ps2x.NewButtonState(PSB_CROSS))               //will be TRUE if button was JUST pressed OR released
-      Serial.println("X just changed");
-    if(ps2x.ButtonReleased(PSB_SQUARE))              //will be TRUE if button was JUST released
-      Serial.println("Square just released");     
 
-    if(ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1)) { //print stick values if either is TRUE
-      Serial.print("Stick Values:");
-      Serial.print(ps2x.Analog(PSS_LY), DEC); //Left stick, Y axis. Other options: LX, RY, RX  
-      Serial.print(",");
-      Serial.print(ps2x.Analog(PSS_LX), DEC); 
-      Serial.print(",");
-      Serial.print(ps2x.Analog(PSS_RY), DEC); 
-      Serial.print(",");
-      Serial.println(ps2x.Analog(PSS_RX), DEC); 
-    }     
   }
   delay(50);  
 }
