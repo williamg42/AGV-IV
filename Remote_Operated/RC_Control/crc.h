e2fd711dad9a803dc25e8be20861763a0116c866
williamg42 <willig2@vt.edu>
2015-08-27 13:32:57 -0400

    Update crc.h

--- Remote_Operated/RC_Control/crc.h
+++ Remote_Operated/RC_Control/crc.h
@@ -71,7 +71,7 @@ typedef unsigned long  crc;
 
 void  crcInit(void);
 crc   crcSlow(unsigned char const message[], int nBytes);
-//crc   crcFast(unsigned char const message[], int nBytes);
+crc   crcFast(unsigned char const message[], int nBytes);
 
 
 #endif /* _crc_h */
